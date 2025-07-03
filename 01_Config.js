9cd17b49c397b5428c5a86a5cfbbc68493df4e5b
Alexander Sakovich <poxagronka@MacBookPro.home>
2025-07-03 11:23:18 +0200

    Commit message test

--- 01_Config.js
+++ 01_Config.js
@@ -1,5 +1,5 @@
 /**
- * Configuration file - ИСПРАВЛЕНО: добавлен APPLOVIN
+ * Configuration file - ИСПРАВЛЕНО: добавлен APPLOVIN тест
  */
 
 var MAIN_SHEET_ID = '1sU3G0HYgv-xX1UGK4Qa_4jhpc7vndtRyKsojyVx9iaE';
